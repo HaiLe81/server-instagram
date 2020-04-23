0d27b9f83ba9bc60886a31e6e1abe43350c3eceb
Glitch (crud-list-book) <none>
2020-04-23 05:40:02 +0000

    validare create user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -55,7 +55,22 @@ module.exports = {
   createPost: (req, res) => {
     try {
       const id = shortid.generate();
-      // console.log('body', req.body)
+      const userInput = req.body.name;
+      var errors = [];
+      if(!userInput){
+        errors.push('User is required')
+      }
+      
+      if(userInput.length > 30 || userInput.length < 2){
+        errors.push('Greater than 2 characters and less than 30 chracters, Try again')
+      }
+      
+      if(errors.length) {
+        res.render("./users/createUser.pug", {
+          errors: errors
+        });
+      }
+        
       db.get("listUser")
         .push({
           id: id,
