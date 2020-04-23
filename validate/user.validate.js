0a0287b4669a5ed987e5e8b0df55688978f55988
Glitch (crud-list-book) <none>
2020-04-23 07:47:39 +0000

    🙈🚶 update code in file validate.js

--- validate/user.validate.js
+++ validate/user.validate.js
@@ -1,20 +1,26 @@
 module.exports = {
   postCreate: (req, res, next) => {
-    const userInput = req.body.name;
-    var errors = [];
-      if(!userInput){
-        errors.push('User is required')
+    try {
+      const userInput = req.body.name;
+      var errors = [];
+      if (!userInput) {
+        errors.push("User is required");
       }
-      
-      if(userInput.length > 30 || userInput.length < 2){
-        errors.push('Greater than 2 characters and less than 30 chracters, Try again')
+
+      if (userInput.length > 30 || userInput.length < 2) {
+        errors.push(
+          "Greater than 2 characters and less than 30 chracters, Try again"
+        );
       }
-      
-      if(errors.length) {
+
+      if (errors.length) {
         res.render("./users/createUser.pug", {
           errors: errors
         });
       }
-    next()
+      next();
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
+};
