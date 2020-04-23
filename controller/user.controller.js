4b839bf7411a9e2b8ca8af7f7ecbfc48aa5618ea
Glitch (crud-list-book) <none>
2020-04-23 06:13:12 +0000

    middleware

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -56,20 +56,6 @@ module.exports = {
     try {
       const id = shortid.generate();
       const userInput = req.body.name;
-      var errors = [];
-      if(!userInput){
-        errors.push('User is required')
-      }
-      
-      if(userInput.length > 30 || userInput.length < 2){
-        errors.push('Greater than 2 characters and less than 30 chracters, Try again')
-      }
-      
-      if(errors.length) {
-        res.render("./users/createUser.pug", {
-          errors: errors
-        });
-      }
         
       db.get("listUser")
         .push({
