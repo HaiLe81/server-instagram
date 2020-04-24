4fcbc7e8da05e2460388116661dcef2b85e4695a
Glitch (crud-list-book) <none>
2020-04-24 06:19:27 +0000

    🧀🛣 update limit  password wrong numbers

--- validate/auth.login.validate.js
+++ validate/auth.login.validate.js
@@ -1,6 +1,6 @@
 const md5 = require("md5"); //md5 is third party libraries so must declare first
-const bcrypt = require("bcrypt");
 const db = require("../db");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 module.exports = {
@@ -10,33 +10,40 @@ module.exports = {
       const email = req.body.email;
       const password = req.body.password;
 
-      var user = db
+      console.log("password", password);
+      console.log("email", email);
+
+      const user = db
         .get("listUser")
-        .find({ email: email })
+        .find({ email: req.body.email })
         .value();
-      let passwordData = user.password;
-      console.log("passworddata", passwordData);
-      console.log("check exist", user);
 
       if (!user) {
         errors.push("User does not exist.");
       } else {
-        // if (user.password !== password) {
-        //   errors.push("Wrong password");
-        // }
+        let passwordData = user.password;
+        let countWrongPassword = user.wrongLoginCount;
+        console.log("countWrongPassword:", countWrongPassword);
+        if (countWrongPassword >= 3) {
+          errors.push(
+            "Enter the wrong password more than the specified number of times. Pleasa comback after a day"
+          );
+        } else {
+          // const hash = bcrypt.hashSync(password, saltRounds);
+          const result = bcrypt.compareSync(password, user.password);
+          if (result === true) {
+            console.log("password dung cmnr");
+          } else {
+            errors.push("Wrong password!");
+            db.get("listUser")
+              .find({ email: email })
+              .assign({ wrongLoginCount: (countWrongPassword += 1) })
+              .write();
+            console.log("countWrongPassword2:", countWrongPassword);
+          }
+        }
       }
 
-      bcrypt.compare(password, user.password, function(err, result) {
-        if (result === true) {
-          console.log("password dung r");
-        } else {
-          errors.push("Wrong password!");
-          console.log("password sai r");
-          console.log('errors1', errors)
-        }
-      });
-
-      console.log('errors2', errors)
       if (errors.length) {
         res.render("./auth/auth.pug", {
           errors: errors,
